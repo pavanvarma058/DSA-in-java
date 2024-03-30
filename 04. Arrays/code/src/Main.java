3cf02a172f6d8b9ffc4e496881b101b6add8b873
Pavan Varma <pavanvarma058@gmail.com>
2024-03-30 23:19:07 +0530

    arrays modified

--- 04. Arrays/code/src/Main.java
+++ 04. Arrays/code/src/Main.java
@@ -3,6 +3,13 @@ public class Main {
 
         // Array Syntax
         // datatype[] variable_name = new datatype[size];
+        // store 5 roll numbers
+//        int[] rnos = new int[5];
+        // or directly
+//        int[] rnos2 = {25, 34, 28, 18, 58};
+
+        int[] ros; // declaration of array. ros is getting defined in the stack
+        ros = new int[5]; // initialisation: actually here object is being created in the (heap memory)
         
     }
 }

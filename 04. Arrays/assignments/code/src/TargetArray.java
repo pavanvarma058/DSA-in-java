be361e230bee64d687de160327cbc7336a96835b
Pavan Varma <pavanvarma058@gmail.com>
2025-01-01 23:33:34 +0530

    solved one problem from array assignment section

---
+++ 04. Arrays/assignments/code/src/TargetArray.java
@@ -0,0 +1,9 @@
+public class TargetArray {
+    // link:   https://leetcode.com/problems/create-target-array-in-the-given-order/description/
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 2, 3, 4};
+        int[] index = {0, 1, 2, 2, 1};
+
+        
+    }
+}

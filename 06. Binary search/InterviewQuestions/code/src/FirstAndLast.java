77624a88d15cd6d5323dac2f86d09d17bafedc40
Pavan Varma <pavanvarma058@gmail.com>
2025-01-16 21:51:43 +0530

    solved FirstAndLastPosition problem from leetcode

---
+++ 06. Binary search/InterviewQuestions/code/src/FirstAndLast.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+
+public class FirstAndLast {
+    // link: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+    public static void main(String[] args) {
+        int[] nums = {5, 7, 7, 7, 8, 8, 10};
+        int target = 7;
+        int[] ans = searchRange(nums, target);
+        System.out.println(Arrays.toString(ans));
+    }
+    public static int[] searchRange(int[] nums, int target) {
+//        int[] ans = {-1, -1};
+//        // check for first occurrence in the target
+//        int startVal = search(nums, target, true);
+//        int endVal = search(nums, target, false);
+//
+//        ans[0] = startVal;
+//        ans[1] = endVal;
+//        return ans;
+
+        int[] ans = {-1, -1};
+        // check for first occurrence in the target
+        ans[0] = search(nums, target, true);
+        if (ans[0] != -1) {
+            ans[1] = search(nums, target, false);
+        }
+        return ans;
+    }
+
+    // this function just returns the index value of target
+    static  int search(int[] nums, int target, boolean findStartIndex){
+        int ans = -1;
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start<=end) {
+            int mid = start + (end - start) / 2;
+            if (target < nums[mid]) {
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                ans = mid;
+                if(findStartIndex){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+}
